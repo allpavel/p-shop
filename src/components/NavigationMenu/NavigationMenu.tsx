53dcef2be354fa630e935348ab6b0a8d3d9ba060
Pavel Alloyarov <allpavel@yandex.ru>
2023-10-05 21:04:55 +0300

    added navigation menu components

--- src/components/NavigationMenu/NavigationMenu.tsx
+++ src/components/NavigationMenu/NavigationMenu.tsx
@@ -1,5 +1,7 @@
 import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react";
 import NavigationMenuPrimitive from "@radix-ui/react-navigation-menu";
+import { ChevronDownIcon } from "@radix-ui/react-icons";
+import { cva } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const NavigationMenu = forwardRef<
@@ -32,9 +34,54 @@ const NavigationMenuList = forwardRef<
   );
 });
 
+const navigationMenuTriggerStyle = cva(
+  "group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50",
+);
+
+const NavigationMenuTrigger = forwardRef<
+  ElementRef<typeof NavigationMenuPrimitive.Trigger>,
+  ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Trigger>
+>(({ className, children, ...props }, ref) => (
+  <NavigationMenuPrimitive.Trigger
+    ref={ref}
+    className={cn(navigationMenuTriggerStyle(), "group", className)}
+    {...props}
+  >
+    {children}
+    <ChevronDownIcon
+      className="relative top-[1px] ml-1 h-3 w-3 transition duration-300 group-data-[state=open]:rotate-180"
+      aria-hidden="true"
+    />
+  </NavigationMenuPrimitive.Trigger>
+));
+
+const NavigationMenuContent = forwardRef<
+  ElementRef<typeof NavigationMenuPrimitive.Content>,
+  ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Content>
+>(({ className, ...props }, ref) => (
+  <NavigationMenuPrimitive.Content
+    ref={ref}
+    className={cn(
+      "left-0 top-0 w-full data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52 md:absolute md:w-auto ",
+      className,
+    )}
+    {...props}
+  />
+));
+
 const NavigationMenuItem = NavigationMenuPrimitive.Item;
+const NavigationMenuLink = NavigationMenuPrimitive.Link;
 
 NavigationMenu.displayName = NavigationMenuPrimitive.Root.displayName;
 NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName;
+NavigationMenuTrigger.displayName = NavigationMenuPrimitive.Trigger.displayName;
+NavigationMenuContent.displayName = NavigationMenuPrimitive.Content.displayName;
 
-export { NavigationMenu, NavigationMenuList, NavigationMenuItem };
+export {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  navigationMenuTriggerStyle,
+  NavigationMenuLink,
+  NavigationMenuContent,
+};
