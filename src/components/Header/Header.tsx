637641201cfdc349fca2cc9ae9babd01bc5aa58f
Pavel Alloyarov <allpavel@yandex.ru>
2023-10-19 21:50:23 +0300

    refactor a navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,15 @@
-import { TempMenu } from "../TempMenu/TempMenu";
+import { HeaderBottom } from "../HeaderBottom/HeaderBottom";
+import { HeaderMiddle } from "../HeaderMiddle/HeaderMiddle";
+import { HeaderTop } from "../HeaderTop/HeaderTop";
 import { Container } from "../ui/Container/Container";
-// import { Temp2 } from "../temp2/Temp2";
 
 export function Header() {
   return (
     <Container>
       <header className="relative m-auto">
-        {/* <HeaderTop />
-      <HeaderMiddle />
-      <HeaderBottom /> */}
-        {/* <Temp2 /> */}
-        <TempMenu />
+        <HeaderTop />
+        <HeaderMiddle />
+        <HeaderBottom />
       </header>
     </Container>
   );
---
+++ src/components/MobileNavigationMenu/MobileNavigationMenu.tsx
@@ -0,0 +1,109 @@
+"use client";
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuTrigger,
+  NavigationMenuContent,
+  navigationMenuTriggerStyle,
+  NavigationMenuLink,
+} from "@/components/ui/NavigationMenu/NavigationMenu";
+import { MdPhoneInTalk, MdMenu } from "react-icons/md";
+import { CityPicker } from "../CityPicker/CItyPicker";
+import { Button } from "../ui/Button/Button";
+import { Container } from "../ui/Container/Container";
+
+export function MobileNavigationBar() {
+  return (
+    <Container>
+      <MdMenu />
+      <NavigationMenu className="pt-2 flex-col lg:hidden">
+        <CityPicker />
+        <NavigationMenuList className="flex-col">
+          <NavigationMenuItem>
+            <Link href={"about"} legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                О нас
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href={"dostavka"} legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Доставка и оплата
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href={"faq"} legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Вопросы и ответы
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href={"feedback"} legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Отзывы
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href={"articles"} legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Статьи
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href={"contacts"} legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Контакты
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Ещё</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuList className="mt-1 flex-col items-start shadow-md">
+                <NavigationMenuItem>
+                  <Link href={"bonuses"} legacyBehavior passHref>
+                    <NavigationMenuLink
+                      className={`${navigationMenuTriggerStyle()} mb-1`}
+                    >
+                      Бонусная программа
+                    </NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <Link href={"companies"} legacyBehavior passHref>
+                    <NavigationMenuLink
+                      className={navigationMenuTriggerStyle()}
+                    >
+                      Для юр. лиц
+                    </NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+        <NavigationMenuList className="flex flex-col min-[1160px]:flex-row">
+          <NavigationMenuItem className={navigationMenuTriggerStyle()}>
+            <MdPhoneInTalk className="text-yellow-500 h-5 w-5 me-1" />
+            <a href="tel:+7(3452)59-49-45">+7 (3452) 59-49-45</a>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Button
+              variant="outline"
+              className="text-yellow-400 px-2 min-[1350px]:px-4"
+            >
+              Заказать звонок
+            </Button>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </Container>
+  );
+}
